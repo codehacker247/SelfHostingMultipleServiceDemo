74579c8154b9464276c3e7cfa0d63341a1ebef4d
Rohit <rohitkumarmandal34@gmail.com>
2022-12-29 19:55:30 +0530

    Add project files.

---
+++ SelfHost/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathServiceLibrary;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using CalcServiceLibrary;
+
+namespace SelfHost
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var servicehost2 = new ServiceHost(typeof(CalcService)); 
+            var servicehost = new ServiceHost(typeof(MathService));
+            
+            ServiceEndpoint BasicHttpEndPoint1 = servicehost.AddServiceEndpoint(
+            typeof(IMathService),
+            new BasicHttpBinding(),
+            "http://localhost:4444/MathService");
+
+            ServiceEndpoint BasicHttpEndPoint2 = servicehost2.AddServiceEndpoint(
+            typeof(ICalcService),
+            new BasicHttpBinding(),
+            "http://localhost:5555/CalcService");
+
+            servicehost.Open();
+            servicehost2.Open();
+
+            Console.WriteLine("Your WCF Service is running");
+            foreach(ServiceEndpoint endpoint in servicehost.Description.Endpoints)
+            {
+                Console.WriteLine("Host 1 Details:- Address : {0} Binding Name : {1}",endpoint.Address.ToString(),endpoint.Binding.Name);
+            }
+            foreach (ServiceEndpoint endpoint2 in servicehost.Description.Endpoints)
+            {
+                Console.WriteLine("Host 2 Details:- Address : {0} Binding Name : {1}", endpoint2.Address.ToString(), endpoint2.Binding.Name);
+            }
+            Console.WriteLine("Press any key to stop your WCF Math Service.");
+            Console.ReadKey();  
+
+            servicehost.Close();    
+            servicehost2.Close();   
+        }
+    }
+}
---
+++ WindowsClient1/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MathServiceLibrary;
+using CalcServiceLibrary;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+namespace WindowsClient1
+{
+    public partial class Form1 : Form
+    {
+        private IMathService channel = null;
+        private ICalcService channel2 = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            var endpoint = new EndpointAddress("http://localhost:4444/MathService");
+            channel = ChannelFactory<IMathService>.CreateChannel(new BasicHttpBinding(), endpoint);
+
+            var endpoint2 = new EndpointAddress("http://localhost:5555/CalcService");
+            channel2 = ChannelFactory<ICalcService>.CreateChannel(new BasicHttpBinding(), endpoint2);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MathServiceLibrary.MyNumbers obj = new MathServiceLibrary.MyNumbers();
+            obj.Number1 = Convert.ToInt32(textBox1.Text);
+            obj.Number2 = Convert.ToInt32(textBox2.Text);    
+            textBox3.Text= channel.Add(obj).ToString(); 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MathServiceLibrary.MyNumbers obj = new MathServiceLibrary.MyNumbers();
+            obj.Number1 = Convert.ToInt32(textBox1.Text);
+            obj.Number2 = Convert.ToInt32(textBox2.Text);
+            textBox3.Text = channel.Subtract(obj).ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            CalcServiceLibrary.MyNumbers obj = new CalcServiceLibrary.MyNumbers();
+            obj.Number1 = Convert.ToInt32(textBox1.Text);
+            obj.Number2 = Convert.ToInt32(textBox2.Text);
+            textBox3.Text = channel2.Multiply(obj).ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            CalcServiceLibrary.MyNumbers obj = new CalcServiceLibrary.MyNumbers();
+            obj.Number1 = Convert.ToInt32(textBox1.Text);
+            obj.Number2 = Convert.ToInt32(textBox2.Text);
+            textBox3.Text = channel2.Divide(obj).ToString();
+        }
+    }
+}
